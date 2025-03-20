c02feee149eec1f135d5e9541b59815c6ca9d459
MsrSDK <s.713193@gmail.com>
2025-03-20 18:12:36 +0900

    ⚗️ API末尾がpilotsの場合を検出

--- devtools.js
+++ devtools.js
@@ -5,8 +5,23 @@ document.addEventListener('mousedown', function(event){
   }
 });
 
+function getUrlLast(url) {
+  const pathname = new URL(url).pathname;
+  return pathname.substring(pathname.lastIndexOf('/') + 1);
+}
+
 chrome.devtools.network.onRequestFinished.addListener(request => {
-  chrome.devtools.inspectedWindow.eval(
-    'console.log("' + request.request.url + '")'
-  );
+  request.getContent((body) => {
+    if(request.request && request.request.url){
+      const urlLast = getUrlLast(request.request.url);
+      if(urlLast == "pilots"){
+        const bodyObj = JSON.parse(body);
+        chrome.devtools.inspectedWindow.eval(
+          // 'console.log("' + JSON.stringify(bodyObj) + '")'
+          'console.log("' + urlLast + '")'
+        );
+      }
+
+    }
+  });
 });
