7e706e8a04e4351281a99d50825d44099b0ec575
MsrSDK <s.713193@gmail.com>
2025-03-22 03:23:05 +0900

    ✨ テーブルのページ変更を検出する

--- content.js
+++ content.js
@@ -1,7 +1,3 @@
-// ページ利ロード時に発火
-document.addEventListener('load', (event) => { getElement(); }, true);
-
-
 function getElement(){
   const elements = document.getElementsByTagName('tbody');
   const message = { "action": "getRecordData" }
@@ -13,9 +9,29 @@ function getElement(){
         return;
       }
       console.log("バックエンドからの応答:", response);
+
+      // 変更を監視するオブザーバーのオプション
+      const config = { childList: true };
+      // 変更が発見されたときに実行されるコールバック関数
+      const callback = (mutationList, observer) => {
+        for (const mutation of mutationList) {
+          if (mutation.type === "childList") {
+            console.log("子ノードが追加または削除されました。");
+          }
+        }
+      };
+
+      // コールバック関数に結びつけられたオブザーバーのインスタンスを生成
+      const observer = new MutationObserver(callback);
+
+      // 対象ノードの設定された変更の監視を開始
+      observer.observe(elements[0], config);
     });
   } else {
     // 要素が存在しない場合の処理
     console.log('要素が見つかりませんでした。');
   }
 }
+
+// ページリロード時に発火
+document.addEventListener('load', (event) => { getElement(); }, true);
